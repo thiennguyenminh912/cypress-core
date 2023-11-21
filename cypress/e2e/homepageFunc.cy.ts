ccb36b28fcf38fcf799f18f65c944c397432140b
DoanLy <lydoan.king@gmail.com>
2023-11-21 15:28:29 +0700

    commit_footer

--- cypress/e2e/homepageFunc.cy.ts
+++ cypress/e2e/homepageFunc.cy.ts
@@ -5,40 +5,51 @@ import { arraysAreEqual } from "helpers/util";
 
 
 describe('Verify home page', () => {
-    it('Logo and title visible', () => {
+    // it('Logo and title visible', () => {
+    //     cy.visit(baseUrl);
+    //     cy.get('#nava img[src="bm.png"]').should('be.visible');
+    //     cy.get('#nava').should('contain.text', 'PRODUCT STORE');
+    // });
+
+    // it('Verify header menu item', () => {
+    //     cy.visit(baseUrl);
+
+    //     // Đăng nhập và đợi cho quá trình đăng nhập hoàn tất
+    //     auth.login({ username: 'lydtt', password: '123456', page: baseUrl });
+    //     cy.wait(5000);
+
+    //     const expectMenu = [
+    //         'Home (current)',
+    //         'Contact',
+    //         'About us',
+    //         'Cart',
+    //         'Log in',
+    //         'Sign up'
+    //     ];
+
+
+    //     cy.get('.some-other-selector')
+    //         .should('be.visible')
+    //         .then(() => {
+    //             // Thực hiện các hành động hoặc kiểm thử khác ở đây
+    //             const items = [];
+    //             homepage.getMenuItemList().each(($menuText) => {
+    //                 items.push($menuText.text());
+    //             })
+    //                 .then(() => {
+    //                     expect(arraysAreEqual(items, expectMenu)).to.equal(true);
+    //                 });
+    //         });
+    // });
+
+    it('Verify footer', () => {
         cy.visit(baseUrl);
-        cy.get('#nava img[src="bm.png"]').should('be.visible');
-        cy.get('#nava').should('contain.text', 'PRODUCT STORE');
-    });
+        const expectedAboutUs = '\n About Us We believe performance needs to be validated at every stage of the software development cycle and our\n open source compatible, massively scalable platform makes that a reality.\n          '
+        //cy.log(expectedAboutUs);
 
-    it('Verify header menu item', () => {
-        cy.visit(baseUrl);
-
-        // Đăng nhập và đợi cho quá trình đăng nhập hoàn tất
-        auth.login({ username: 'lydtt', password: '123456', page: baseUrl });
-        cy.wait(5000);
-
-        const expectMenu = [
-            'Home (current)',
-            'Contact',
-            'About us',
-            'Cart',
-            'Log in',
-            'Sign up'
-        ];
-
-
-        cy.get('.some-other-selector')
-            .should('be.visible')
-            .then(() => {
-                // Thực hiện các hành động hoặc kiểm thử khác ở đây
-                const items = [];
-                homepage.getMenuItemList().each(($menuText) => {
-                    items.push($menuText.text());
-                })
-                    .then(() => {
-                        expect(arraysAreEqual(items, expectMenu)).to.equal(true);
-                    });
-            });
+        cy.get('#fotcont .caption').first().then(($columnitem1) => {
+            cy.wrap($columnitem1)
+            //  expect(actualAboutUs).to.equal(expectedAboutUs);
+        });
     });
 });
