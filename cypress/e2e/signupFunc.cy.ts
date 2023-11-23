9db13ffff93d45dbc38c3c120cbbd25792fecc44
thiennguyen <thiennguyen@dhttech.com>
2023-11-23 14:55:14 +0700

    resuse get alert component

--- cypress/e2e/signupFunc.cy.ts
+++ cypress/e2e/signupFunc.cy.ts
@@ -2,27 +2,26 @@ import { baseUrl } from "helpers/constant";
 import auth from "helpers/signup";
 import { generateRandomUser } from "helpers/util";
 
-
 const SIGN_UP = {
-    username: generateRandomUser(10),
-    password: "123456"
-}
-
+  username: generateRandomUser(10),
+  password: "123456",
+};
 
 describe("Signup Function", () => {
-    //Valid user+password
-    it('Signup successfully!', () => {
-        auth.signup({ username: SIGN_UP.username, password: SIGN_UP.password, page: baseUrl });
-        cy.on('window:alert', alert => {
-            expect(alert).to.contains("Sign up successful.")
-        })
+  //Valid user+password
+  it("Signup successfully!", async () => {
+    auth.signup({
+      username: SIGN_UP.username,
+      password: SIGN_UP.password,
+      page: baseUrl,
+    });
+    const alertText = await auth.getAlert();
+    expect(alertText).to.contains("Sign up successful.");
+  });
 
-    })
-
-    it('Signup with user already exist!', () => {
-        auth.signup({ username: 'lydtt', password: '123456', page: baseUrl });
-        cy.on('window:alert', alert => {
-            expect(alert).to.contains("This user already exist.")
-        })
-    })
+  it("Signup with user already exist!", async () => {
+    auth.signup({ username: "lydtt", password: "123456", page: baseUrl });
+    const alertText = await auth.getAlert();
+    expect(alertText).to.contains("This user already exist.");
+  });
 });
--- cypress/helpers/signup.ts
+++ cypress/helpers/signup.ts
@@ -1,18 +1,18 @@
 const auth = {} as IAuthFunc;
 
 auth.getAlert = () => {
-  return cy.on('window:alert', alert)
-}
-
-
+  return new Cypress.Promise((resolve) => {
+    cy.on("window:alert", resolve);
+  });
+};
 
 auth.signup = (inputs) => {
-  const { page, username, password } = inputs
+  const { page, username, password } = inputs;
   cy.visit(page);
-  cy.get('#signin2').click();
+  cy.get("#signin2").click();
   cy.wait(2000);
-  cy.get('#sign-username').type(username);
-  cy.get('#sign-password').type(password);
+  cy.get("#sign-username").type(username);
+  cy.get("#sign-password").type(password);
   cy.get('[onclick="register()"]').click({ force: true });
   cy.wait(5000);
 };
